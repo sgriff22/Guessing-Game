386c4ed6efad211d4ceaebcec146238d14050876
Stacy Griffy <sgriffy2@gmail.com>
2024-08-27 09:02:13 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("🔢 Welcome to the Number Guessing Game!❓");
+   
+int guess = GetValidIntegerInput("Guess a Secret number ");
+Console.WriteLine($"🎯 You guessed: {guess}");
+
+
+int GetValidIntegerInput(string prompt)
+{
+    while (true)
+    {
+        Console.WriteLine(prompt);
+        // Take the user's guess as input
+        string? input = Console.ReadLine();
+
+        // Handle null input by setting to an empty string
+        input ??= string.Empty;
+
+        try
+        {
+            return int.Parse(input);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("❌ Invalid input. Please enter a valid integer.");
+        }
+    }
+}
